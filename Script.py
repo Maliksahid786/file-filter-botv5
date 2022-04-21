04f64b8548c0a58f758b4eaf912878411c91087a
malik7983 <103699299+malik7983@users.noreply.github.com>
2022-04-21 20:23:17 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,6 +1,4 @@
 class script(object):
-    START_TXT = """𝙷𝙴𝙻𝙾 {},
-𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href=https://t.me/{}>{}</a>, 𝙸 𝙲𝙰𝙽 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝙼𝙾𝚅𝙸𝙴𝚂, 𝙹𝚄𝚂𝚃 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 𝙰𝙽𝙳 𝙴𝙽𝙹𝙾𝚈 😍"""
     HELP_TXT = """𝙷𝙴𝚈 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
     ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
@@ -105,3 +103,15 @@ Added By - {}
 ID - <code>{}</code>
 Name - {}
 """
+    START_TXT = """<b>Hay {},
+
+I'm ⚡️ Powerful Auto-Filter Bot...
+ 
+😎 You Can Use Me As A Auto-filter in Your Group ....
+
+Its Easy To Use Me; Just Add Me To Your Group As Admin, Thats All, i will Provide Movies There...😎
+
+⚠️ More Help Check Help Button Below
+
+©️Mantained Bʏ  @sahid_malik</b>"""
+
