b0d5f7025d0a27fd68d853b1bff9b2ec0e08d119
Maliksahid786 <105342787+Maliksahid786@users.noreply.github.com>
2022-05-16 11:11:55 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/malik7983/file-filter-bot.git /file-filter-bot
+  git clone https://github.com/Maliksahid786/file-filter-botv5.git /file-filter-botv5
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /file-filter-bot
+  git clone $UPSTREAM_REPO /file-filter-botv5
 fi
-cd /file-filter-bot
+cd /file-filter-botv5
 pip3 install -U -r requirements.txt
 echo "Starting Bot....💥"
 python3 bot.py
