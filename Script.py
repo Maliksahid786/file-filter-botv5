f23c22646c2ce88d47f95dfd1b262e378c8e2877
Myrenpo <105908597+Myrenpo@users.noreply.github.com>
2022-05-28 21:26:47 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -2,17 +2,17 @@ class script(object):
     HELP_TXT = """𝙷𝙴𝚈 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
     ABOUT_TXT = """<b>✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}</b>
-✯ <b>𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/sahid_nalik>Sahid malik</a>
+✯ <b>𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/dark_webos</a>
 ✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
 ✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
 ✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
 ✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
 ✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]
-✯ updates channel: <a href=https://t.me/m_house786>CLICK HERE</a>
-✯ Subscribe you tube channel: <a href=<a href=https://youtube.com/channel/UCPaHDqWf3D3w2nxb8p3sr4A>CLICK HERE</a></b>""" 
+✯ updates channel: <a href=https://t.me/hub1234ggmovie>CLICK HERE</a>
+✯ Subscribe channel: <a href=<a href=https://t.me/+_X48DP2opzE0MDY1>CLICK HERE</a></b>""" 
     SOURCE_TXT = """<b>NOTE:</b>
 - <b>Movies house is a open source project. 
-- Source 👉 <a href=https://t.me/malik_bots/7>CLICK HERE</a></b>
+- Source 👉 <a href=https://t.me/+_X48DP2opzE0MDY1/7>CLICK HERE</a></b>
 
 <b>DEVS:</b>
 - <a href=https://t.me/sahid_malik>Sahid malik</a>"""
@@ -21,7 +21,6 @@ class script(object):
 - Filter is the feature were users can set automated replies for a particular keyword and EvaMaria will respond whenever a keyword is found the message
 
 <b>NOTE:</b>
-1. movies house 🏠 have admin privillage.
 2. only admins can add filters in a chat.
 3. alert buttons have a limit of 64 characters.
 
