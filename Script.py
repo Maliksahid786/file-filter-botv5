0e9e247866cde50defb65babe04407b036c2cd22
LasithChamik <106394753+LasithChamik@users.noreply.github.com>
2022-06-05 22:11:29 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -18,12 +18,12 @@ class script(object):
 - <a href=https://t.me/sahid_malik>Sahid malik</a>"""
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 
-- Filter is the feature were users can set automated replies for a particular keyword and EvaMaria will respond whenever a keyword is found the message
+- Filter is the Feature were users can set automated replies for a particular keyword and EvaMaria will respond whenever a keyword is found the message
 
-<b>NOTE:</b>
-1. movies house 🏠 have admin privillage.
-2. only admins can add filters in a chat.
-3. alert buttons have a limit of 64 characters.
+<b>⚠️️ NOTE:</b>
+1. Cinema World 🏠 have admin Privillage.
+2. Only admins can add filters in a Chat.
+3. Alert buttons have a limit of 64 Characters.
 
 <b>Commands and Usage:</b>
 • /filter - <code>add a filter in chat</code>
@@ -32,11 +32,11 @@ class script(object):
 • /delall - <code>delete the whole filters in a chat (chat owner only)</code>"""
     BUTTON_TXT = """Help: <b>Buttons</b>
 
-- movies house Supports both url and alert inline buttons.
+- Cinema World supports both url and alert inline buttons.
 
-<b>NOTE:</b>
-1. Telegram will not allows you to send buttons without any content, so content is mandatory.
-2. movies house supports buttons with any telegram media type.
+<b>⚠️️ NOTE:</b>
+1. Telegram will not allows you to send buttons without any content, so content is Mandatory.
+2. Movies house supports buttons with any telegram media type.
 3. Buttons should be properly parsed as markdown format
 
 <b>URL buttons:</b>
@@ -46,17 +46,17 @@ class script(object):
 <code>[Button Text](buttonalert:This is an alert message)</code>"""
     AUTOFILTER_TXT = """Help: <b>Auto Filter</b>
 
-<b>NOTE:</b>
-1. Make me the admin of your channel if it's private.
-2. make sure that your channel does not contains camrips, porn and fake files.
-3. Forward the last message to me with quotes.
- I'll add all the files in that channel to my db."""
+<b>⚠️️ NOTE:</b>
+1. Make me the Admin of your channel if it's private.
+2. Make sure that your channel does not contains camrips, porn and fake files.
+3. Forward the last message to me with Quotes.
+ I'll add all the files in that channel to My DB."""
     CONNECTION_TXT = """Help: <b>Connections</b>
 
 - Used to connect bot to PM for managing filters 
-- it helps to avoid spamming in groups.
+- It helps to avoid spamming in Groups.
 
-<b>NOTE:</b>
+<b>⚠️️ NOTE:</b>
 1. Only admins can add a connection.
 2. Send <code>/connect</code> for connecting me to ur PM
 
@@ -66,8 +66,8 @@ class script(object):
 • /connections - <code>list all your connections</code>"""
     EXTRAMOD_TXT = """Help: <b>Extra Modules</b>
 
-<b>NOTE:</b>
-these are the extra features of Eva Maria
+<b>⚠️️ NOTE:</b>
+These are the Extra Features of Eva Maria
 
 <b>Commands and Usage:</b>
 • /id - <code>get id of a specified user.</code>
@@ -76,7 +76,7 @@ these are the extra features of Eva Maria
 • /search  - <code>get the film information from various sources.</code>"""
     ADMIN_TXT = """Help: <b>Admin mods</b>
 
-<b>NOTE:</b>
+<b>⚠️️ NOTE:</b>
 This module only works for my admins
 
 <b>Commands and Usage:</b>
@@ -91,12 +91,12 @@ This module only works for my admins
 • /unban  - <code>to unban a user.</code>
 • /channel - <code>to get list of total connected channels</code>
 • /broadcast - <code>to broadcast a message to all users</code>"""
-    STATUS_TXT = """<b>★ 𝚃𝙾𝚃𝙰𝙻 𝙵𝙸𝙻𝙴𝚂: <code>{}</code>
-★ 𝚃𝙾𝚃𝙰𝙻 𝚄𝚂𝙴𝚁𝚂: <code>{}</code>
-★ 𝚃𝙾𝚃𝙰𝙻 𝙲𝙷𝙰𝚃𝚂: <code>{}</code>
-★ 𝚄𝚂𝙴𝙳 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱
-★ 𝙵𝚁𝙴𝙴 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱
-★ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/sahid_nalik>Sahid malik</a></b> """
+    STATUS_TXT = """<b>◇ Total Files : <code>{}</code>
+◇ Total Users : <code>{}</code>
+◇ Total Chats : <code>{}</code>
+◇ Used Storage : <code>{}</code> 𝙼𝚒𝙱
+◇ Free Storage : <code>{}</code> 𝙼𝚒𝙱
+◇ Creator : <a href=https://t.me/Cinema_World_Owner>Lasith Chamika</a></b> """
     LOG_TEXT_G = """#NewGroup
 Group = {}(<code>{}</code>)
 Total Members = <code>{}</code>
@@ -107,11 +107,13 @@ ID - <code>{}</code>
 Name - {}
 """
 
-    START_TXT = """<b>Hay {},
+    START_TXT = """<b>Hey {},
 
-I'm ⚡️ Powerful Auto-Filter Bot...
-😎 You Can Use Me As A Auto-filter in Your Group ....
-Its Easy To Use Me; Just Add Me To Your Group As Admin, Thats All, i will Provide Movies There...😎
-⚠️ More Help Check Help Button Below
+I'm V5 Powerful Auto-Filter Bot...⚡
+You can use me as a Auto-Filter in your Group. Its Easy to use me : Just add me to your Group as Admin, Thats all, I Will Provide Movies There...
 
-©️Mantained Bʏ  @sahid_malik</b>"""
+⚠️ More help click Help button Below
+
+◇──────────────────◇
+
+©️ Maintained By : @Cinema_World_Owner</b>"""
