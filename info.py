6ced30dd684b9b377455bc15040fba3c7f1df731
technical_aks <105633032+technicalaks@users.noreply.github.com>
2022-12-04 11:08:12 +0530

    Update info.py

--- info.py
+++ info.py
@@ -32,9 +32,9 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://trialbotmongodb:trialbotmongodb@cluster0.ecrgrzp.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "trialbotmongodb")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'trialbotmongodb')
+DATABASE_URI = environ.get('DATABASE_URI', "")
+DATABASE_NAME = environ.get('DATABASE_NAME', "")
+COLLECTION_NAME = environ.get('COLLECTION_NAME', '')
 
 # Others
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', -1001769642119))
